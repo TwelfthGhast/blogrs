5b6117e5b8acff2fee62d243ee99658322c0df81
TwelfthGhast <twelfthghast@gmail.com>
2023-11-13 08:24:48 +1100

    Add support for static files + Downgrade from bootstrap v5.3.0 -> v5.0.2

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use axum::{
 use md::MarkDownRouteHandler;
 mod bootstrap_parser;
 mod md;
+use tower_http::services::ServeDir;
 
 #[derive(Clone)]
 struct AppState {
@@ -37,10 +38,12 @@ async fn main() {
     let blog_routes = Router::new()
         .route("/", get(feed_handler))
         .fallback(blog_handler);
+    let static_serve_dir = ServeDir::new("static");
     // build our application with a single route
     let app = Router::new()
         .nest("/blog", blog_routes)
         .route("/", get(feed_handler))
+        .nest_service("/static", static_serve_dir)
         .fallback(fallback)
         .with_state(state);
 
