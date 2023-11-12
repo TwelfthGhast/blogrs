18958faf3c1f88a7c0d7e1081cce20b4c1c4bf51
TwelfthGhast <twelfthghast@gmail.com>
2023-11-13 08:47:09 +1100

    Additional CSS/Styling cleanup

--- src/bootstrap_parser.rs
+++ src/bootstrap_parser.rs
@@ -1,8 +1,23 @@
-use pulldown_cmark::{CowStr, Event, Tag};
+use pulldown_cmark::{CodeBlockKind, CowStr, Event, Tag};
 
 pub fn bootstrap_mapper(event: Event) -> Event {
     match event {
-        Event::Start(Tag::Paragraph) => Event::Html(CowStr::Borrowed("<p class=\"text-body\">")),
+        Event::Start(Tag::Paragraph) => Event::Html(CowStr::Borrowed("<p class=\"text-white\">")),
+        Event::Start(Tag::CodeBlock(info)) => match info {
+            CodeBlockKind::Fenced(info) => {
+                let lang = info.split(' ').next().unwrap();
+                if lang.is_empty() {
+                    Event::Html(CowStr::Borrowed("<pre><code class=\"text-white\">"))
+                } else {
+                    Event::Html(CowStr::Borrowed(
+                        "<pre><code class='language-python text-white'>",
+                    ))
+                }
+            }
+            CodeBlockKind::Indented => {
+                Event::Html(CowStr::Borrowed("<pre><code class=\"text-white\">"))
+            }
+        },
         _ => event,
     }
 }
